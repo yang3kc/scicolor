d3d211b807285e2b5c77b285e49404da11ba47be
Kaicheng Yang <yangkc@iu.edu>
2024-08-14 11:41:40 -0400

    add: @ path

--- colorpicker/vite.config.js
+++ colorpicker/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/scicolor/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
