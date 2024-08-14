268b5930e7ca83a8a4e2565de9c455cd3c420602
Kaicheng Yang <yangkc@iu.edu>
2024-08-14 11:36:38 -0400

    fix: the theme

--- colorpicker/tailwind.config.js
+++ colorpicker/tailwind.config.js
@@ -5,4 +5,28 @@ export default {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            maxWidth: "none"
+          }
+        }
+      })
+    }
+  },
+  daisyui: {
+    themes: [
+      {
+        kevin: {
+          ...require("daisyui/src/theming/themes")["autumn"],
+          "primary": "#ac4142",
+          "background-color": "white"
+        }
+      }
+    ],
+    styled: true,
+    base: true
+  }
 }
