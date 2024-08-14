daf7bce95eff0a14a5f17fbe90548246763abad6
yang3kc <yang3kc@gmail.com>
2024-08-14 12:36:22 -0400

    add: test for data loading

--- colorpicker/src/components/ColorPicker.vue
+++ colorpicker/src/components/ColorPicker.vue
@@ -1,9 +1,22 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+
+const colors = ref({});
+
+onMounted(() => {
+  fetch("/scicolor/colors/colors.json")
+    .then((response) => response.json())
+    .then((data) => {
+      colors.value = data;
+      console.log(colors.value);
+    });
+});
 
 const hello = ref("Hello World");
 </script>
 
 <template>
+  <div class="container">
     <h1>{{ hello }}</h1>
+  </div>
 </template>
