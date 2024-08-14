299f42058d59762bf35868abc6495ba3dbfcaeee
yang3kc <yang3kc@gmail.com>
2024-08-14 12:36:14 -0400

    add: router

---
+++ colorpicker/src/components/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ColorPicker from "@/components/ColorPicker.vue";
+</script>
+
+<template>
+  <div class="container mx-auto max-w-screen-xl">
+    <h1 class="text-2xl font-bold">Color Picker</h1>
+    <ColorPicker />
+  </div>
+</template>
