4dd6e7b6094865d2de7b27e6095ca7495f33ffe9
yang3kc <yang3kc@gmail.com>
2024-08-14 12:34:03 -0400

    add: router

--- colorpicker/src/App.vue
+++ colorpicker/src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import ColorPicker from "@/components/ColorPicker.vue";
 </script>
 
 <template>
-  <div class="container mx-auto max-w-screen-lg">
-    <h1>Color Picker</h1>
-    <ColorPicker />
-  </div>
+  <router-view />
 </template>
 
 <style scoped>
