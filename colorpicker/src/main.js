5167e9368ae0b07fd6a80932ce8a27fefa0d2ce4
yang3kc <yang3kc@gmail.com>
2024-08-14 12:33:56 -0400

    add: router

--- colorpicker/src/main.js
+++ colorpicker/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './main.css'
 import App from './App.vue'
+import router from './router'
+import './main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
